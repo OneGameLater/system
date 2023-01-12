444307ff95a66336004c0d0f43089b06e11bbcc4
OneGameLater <git@onegamelater.com>
2023-01-12 22:20:47 +0000

    initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'One Game Later',
+  outDir: '../public',
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/onegamelater/system' }
+    ]
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+One Game Later aims to be an MIT licensed tabletop RPG fully compatible with the dragon game.
+
+Watch this space.
